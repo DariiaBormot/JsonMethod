23bbee73233f94ac8f551473643b52dfdefc95b4
Daria <dariiabormot@gmail.com>
2020-02-03 15:44:40 +0200

    custom json method

---
+++ Json_Exercise/Json_Exercise/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("John","Wick","Student", 90);
+
+            var myJString = GetJsonString(student1);
+            Console.WriteLine(myJString);
+
+            var myDeserializedString = JsonConvert.DeserializeObject(myJString);
+            Console.WriteLine(myDeserializedString);
+            Console.ReadKey();
+        }
+        public static string GetJsonString(object obj)
+        {
+            var type = obj.GetType();
+            var properties = type.GetProperties();
+            var result = string.Empty;
+
+            foreach (var property in properties)
+            {
+                var attribute = property.GetCustomAttribute(typeof(IgnorePropertyAttribute));
+
+                    if (attribute == null)
+                    {
+                        var value = property.GetValue(obj);
+                        if (value is string)
+                        {
+                            result += $"\"{property.Name}\":\"{value}\",";
+                        }
+                        else
+                        {
+                            result += $"\"{property.Name}\":{value}";
+                        }
+                    }
+                            
+            }
+            return $"{{{result}}}";
+        }
+    }
+}
---
+++ Json_Exercise/Json_Exercise/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json_Exercise
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        [IgnoreProperty]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Student(string name, string lastName, string status, int age)
+        {
+            Name = name;
+            Status = status;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
